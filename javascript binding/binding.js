58016cb94cf1a33105b0459cb74ea743e174061b
MuhammadTalha <91981004+M-TALH007@users.noreply.github.com>
2023-06-09 11:11:03 +0500

    change

--- javascript binding/binding.js
+++
@@ -1,9 +0,0 @@
- function hi(){
-    console.log("hi");
-    var inp = document.getElementById("input");
-    var output = document.getElementById("output");
-    output.innerHTML = inp.value;
-
-    
-
-}
